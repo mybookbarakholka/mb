4a094e48c42ac65c5fd4a2e44ba0faf63f676d60
Vladyslava <vlkoshova121@gmail.com>
2023-09-30 16:26:04 +0300

    fb

--- constants/books.js
+++ constants/books.js
@@ -11,7 +11,7 @@ let booksbase = [
                 condition: "6/10",
                 link: "@vvvv4804",
                 comment: "Пожовклі сторінки. Зустрічі немає, в Полтаві можлива педача через поштомат нової пошти. В інші міста відправка будь якою поштою, можливе замовлення, через олх",
-                postLink: "https://www.instagram.com/my.book_barakholka/?img_index=1",
+                postLink: "https://www.instagram.com/p/Cx0HbfhMts-/",
                 authorLink: "https://www.instagram.com/vvvv4804/"
             }]
     },
